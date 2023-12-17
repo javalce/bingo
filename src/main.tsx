df3d9e456386bc3db0310d3d848a41a9e3b55dd4
Javier Valero Cejudo <javalce29@gmail.com>
2023-12-17 19:49:50 +0100

    Reset project

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+
+import '@unocss/reset/tailwind-compat.css';
+import 'virtual:uno.css';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
